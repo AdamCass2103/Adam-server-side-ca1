d88b5efcb0ae74775a7b90758129e8b53f55e817
Adam Cassidy <D00265095@student.dkit.ie>
2025-03-23 21:08:51 +0000

    Started Dreadnots blog layout

--- dreadnots_gaa_blog/database/seeders/PlayerSeeder.php
+++ dreadnots_gaa_blog/database/seeders/PlayerSeeder.php
@@ -2,17 +2,32 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
 use App\Models\Player;
 
-public function run()
+class PlayerSeeder extends Seeder
 {
-    Player::create([
-        'name' => 'John O\'Reilly',
-        'age' => 24,
-        'position' => 'Forward',
-        'user_id' => 1
-    ]);
+    public function run()
+    {
+        Player::create([
+            'name' => 'John O\'Reilly',
+            'age' => 24,
+            'position' => 'Forward',
+            'user_id' => 1
+        ]);
+
+        Player::create([
+            'name' => 'Mick Donnelly',
+            'age' => 28,
+            'position' => 'Midfielder',
+            'user_id' => 1
+        ]);
+
+        Player::create([
+            'name' => 'Brian Casey',
+            'age' => 22,
+            'position' => 'Goalkeeper',
+            'user_id' => 1
+        ]);
+    }
 }
