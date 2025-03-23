388973f92a7a75522dabc43c87c3a1dcb93b7768
Adam Cassidy <D00265095@student.dkit.ie>
2025-03-23 21:38:18 +0000

    Started Dreadnots blog layout

--- dreadnots_gaa_blog/resources/views/posts/index.blade.php
+++ dreadnots_gaa_blog/resources/views/posts/index.blade.php
@@ -13,8 +13,19 @@
         @foreach ($posts as $post)
             <li>
                 <a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a>
+                <a href="{{ route('posts.edit', $post->id) }}">Edit</a>
+                <form action="{{ route('posts.destroy', $post->id) }}" method="POST" style="display:inline;">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit">Delete</button>
+                </form>
             </li>
         @endforeach
     </ul>
+
+    <!-- Add pagination -->
+    <div>
+        {{ $posts->links() }}
+    </div>
 </body>
 </html>
