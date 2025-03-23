defc1889d160c0725b346e537732efbe2013c2f6
Adam Cassidy <D00265095@student.dkit.ie>
2025-03-23 14:50:14 +0000

    Started Dreadnots blog layout

--- dreadnots_gaa_blog/routes/web.php
+++ dreadnots_gaa_blog/routes/web.php
@@ -1,5 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\PlayerController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::resource('posts', PostController::class);
 Route::resource('players', PlayerController::class);
