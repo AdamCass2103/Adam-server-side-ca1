6c06471bfd05e1f0f2228efa2d6c74ed44e86220
Adam Cassidy <D00265095@student.dkit.ie>
2025-03-23 21:08:10 +0000

    Started Dreadnots blog layout

--- dreadnots_gaa_blog/database/seeders/PostSeeder.php
+++ dreadnots_gaa_blog/database/seeders/PostSeeder.php
@@ -2,17 +2,23 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
 use App\Models\Post;
 
-public function run()
+class PostSeeder extends Seeder
 {
-    Post::create([
-        'title' => 'Welcome to Dreadnots GAA Blog',
-        'content' => 'This is the first post for Dreadnots GAA team. Stay tuned for updates!',
-        'user_id' => 1
-    ]);
-}
+    public function run()
+    {
+        Post::create([
+            'title' => 'Welcome to Dreadnots GAA Blog',
+            'content' => 'This is the first post for Dreadnots GAA team. Stay tuned for updates!',
+            'user_id' => 1
+        ]);
 
+        Post::create([
+            'title' => 'Match Day Recap',
+            'content' => 'Dreadnots GAA won by 3 points against their rivals!',
+            'user_id' => 1
+        ]);
+    }
+}
