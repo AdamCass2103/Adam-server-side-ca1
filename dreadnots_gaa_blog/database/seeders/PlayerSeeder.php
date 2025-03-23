896c563e21cb31de932bef2c543c24c9c577f728
Adam Cassidy <D00265095@student.dkit.ie>
2025-03-23 21:17:34 +0000

    Started Dreadnots blog layout

--- dreadnots_gaa_blog/database/seeders/PlayerSeeder.php
+++ dreadnots_gaa_blog/database/seeders/PlayerSeeder.php
@@ -4,27 +4,46 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Player;
+use Carbon\Carbon;
 
 class PlayerSeeder extends Seeder
 {
-    public function run()
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
     {
         Player::create([
-            'name' => "John O'Reilly",
+            'name' => 'John O\'Reilly',
             'age' => 24,
             'position' => 'Forward',
-            'goals' => 10,
-            'assists' => 5,
-            'user_id' => 1, // Make sure this user exists!
+            'goals' => 12,
+            'assists' => 7,
+            'user_id' => 1, // Make sure user with ID 1 exists
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
 
         Player::create([
-            'name' => "Paul Murphy",
+            'name' => 'Paul Murphy',
             'age' => 28,
             'position' => 'Midfielder',
-            'goals' => 4,
-            'assists' => 8,
+            'goals' => 5,
+            'assists' => 14,
             'user_id' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        Player::create([
+            'name' => 'David Byrne',
+            'age' => 22,
+            'position' => 'Goalkeeper',
+            'goals' => 0,
+            'assists' => 1,
+            'user_id' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
     }
 }
